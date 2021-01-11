2223014c51577336fb5898257a69ee7f153ea732
Julia Hansi <ju851han@htwg-konstanz.de>
2021-01-11 18:23:00 +0100

    added mockito test class (exercise 2b)

---
+++ ue05/src/test/java/VendingMachineImpl_MockitoTest.java
@@ -0,0 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+/**
+ * Aufgabe 2b
+ */
+public class VendingMachineImpl_MockitoTest {
+    @Mock
+    CashBox cashBox;
+    @Mock
+    Box box;
+    @InjectMocks
+    VendingMachineImpl vendingMachine;
+
+    @Before
+    public void setUp() {
+    cashBox = Mockito.mock(CashBox.class);
+    box = Mockito.mock(Box.class);
+    vendingMachine = new VendingMachineImpl(cashBox,new Box[]{box});
+    }
+
+    @Test
+    public void test_selectItem() throws BoxEmptyException, NotEnoughMoneyException {
+        Mockito.when(cashBox.getCurrentAmount()).thenReturn(42);
+
+        Mockito.when(box.isEmpty()).thenReturn(Boolean.FALSE);
+        Mockito.when(box.getPrice()).thenReturn(42);
+
+        vendingMachine.selectItem(0);
+        Mockito.verify(box).releaseItem();
+        Mockito.verify(cashBox).withdraw(42);
+    }
+}
